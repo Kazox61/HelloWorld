a346a70eaa2453f84cbd63f8291d5dd75c7d8058
Kazox61 <pawelka00florian@gmail.com>
2026-02-15 13:16:01 +0100

    Updates Physics.

--- Client/core/view_behavior/BoxColliderBehavior.cs
+++ Client/core/view_behavior/BoxColliderBehavior.cs
@@ -2,6 +2,7 @@
 using Godot;
 using HelloWorld.addons.massive_godot_integration.view_synchronizer;
 using Massive;
+using Massive.Common;
 using Massive.Physics.Components;
 
 namespace HelloWorld.Client.Core;
@@ -9,15 +10,18 @@ namespace HelloWorld.Client.Core;
 [GlobalClass]
 public partial class BoxColliderBehavior : EntityBehaviour {
 	private DataSet<BoxCollider> _boxColliders;
+	private DataSet<Transform> _transforms;
 	private Entity _entity;
 	
 	public override void OnEntityAssigned(Entity entity) {
 		_entity = entity;
 		_boxColliders = _entity.World.DataSet<BoxCollider>();
+		_transforms = _entity.World.DataSet<Transform>();
 		Update();
 	}
 	public override void OnEntityRemoved() {
 		_boxColliders = null;
+		_transforms = null;
 		_entity = Entity.Dead;
 	}
 
@@ -31,12 +35,17 @@ public partial class BoxColliderBehavior : EntityBehaviour {
 			return;
 		}
 		
+		if (!_transforms.Has(_entity.Id)) {
+			return;
+		}
+		
 		var boxCollider = _boxColliders.Get(_entity.Id);
+		var transform = _transforms.Get(_entity.Id);
 		var aabb = new Aabb(
 			new Vector3(
-				boxCollider.Center.X.ToFloat() - boxCollider.HalfExtents.X.ToFloat(), 
-				boxCollider.Center.Y.ToFloat() - boxCollider.HalfExtents.Y.ToFloat(), 
-				boxCollider.Center.Z.ToFloat() - boxCollider.HalfExtents.Z.ToFloat()
+				transform.Position.X.ToFloat() - boxCollider.HalfExtents.X.ToFloat(), 
+				transform.Position.Y.ToFloat() - boxCollider.HalfExtents.Y.ToFloat(), 
+				transform.Position.Z.ToFloat() - boxCollider.HalfExtents.Z.ToFloat()
 			),
 			new Vector3(boxCollider.HalfExtents.X.ToFloat() * 2, boxCollider.HalfExtents.Y.ToFloat() * 2, boxCollider.HalfExtents.Z.ToFloat() * 2)
 		);
